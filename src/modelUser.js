5959052fdcdc48cf53d6932d52d9b0e5442d6c96
Martin Morães <martin.moraes@outlook.com>
2021-09-22 08:18:40 -0300

    fix: aplicado freezeTableName

--- src/modelUser.js
+++ src/modelUser.js
@@ -1,18 +1,24 @@
 const { DataTypes } = require('sequelize');
 const database = require('./db');
 
-const ModelUser = database.define('user', {
-  idUser: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const ModelUser = database.define(
+  'user',
+  {
+    idUser: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(80),
+      allowNull: false,
+    },
+    cpf: { type: DataTypes.CHAR },
   },
-  name: {
-    type: DataTypes.STRING(80),
-    allowNull: false,
-  },
-  cpf: { type: DataTypes.CHAR },
-});
+  {
+    freezeTableName: true,
+  }
+);
 
 module.exports = { ModelUser };
--- test/repositoryQuery.test.js
+++ test/repositoryQuery.test.js
@@ -4,12 +4,12 @@ describe('RepositoryQuery', () => {
   const repositoryQuery = new RepositoryQuery();
 
   it('Query Type', async () => {
-    const received = await repositoryQuery.queryType('select * from users');
+    const received = await repositoryQuery.queryType('select * from user');
     console.log(received);
     expect(received).toEqual(expect.arrayContaining([]));
   });
   it('Query', async () => {
-    const received = await repositoryQuery.query('select * from users');
+    const received = await repositoryQuery.query('select * from user');
     console.log(received);
     expect(received).toEqual(expect.arrayContaining([]));
   });
